2879baba3e1df1c7e0058943c543a7363df54286
NguyenHoangDuyKhangZ <khang.nguyenhoangduyz2311@gmail.com>
2024-05-04 11:21:53 +0700

    V3

---
+++ Question3/src/main/java/com/mycompany/question3/Question3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.question3;
+
+/**
+ *
+ * @author Student
+ */
+public class Question3 {
+  public static void main(String[] args) {
+        Company company = new Company(10);
+        company.inputEmployees();
+        company.printSalarySheet();
+        company.printEmployeesBySalary();
+        company.highestSalaryEmployee();
+    }
+    
+}
